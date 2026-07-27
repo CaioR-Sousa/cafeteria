3de62e7c3c7733744de04b1645191445a5edba06
caio5511 <caio.sousa.2504@gmail.com>
2026-07-27 20:27:25 -0300

    Initial commit

---
+++ cafeteria.js
@@ -0,0 +1,68 @@
+const btToggle = document.querySelector(".menu-toggle")
+const menu = document.querySelector(".menu")
+const menuLinks = document.querySelectorAll(".menu a")
+const btVoltar = document.querySelector(".back-to-top")
+const animarSecao = document.querySelectorAll(".animar");
+const secoes = document.querySelectorAll("main section")
+
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("ativo")
+
+            observer.unobserve(entry.target)
+        }
+    })
+});
+
+animarSecao.forEach((secao) => {
+    observer.observe(secao)
+})
+
+window.addEventListener("scroll", () => {
+    secoes.forEach((secao) => {
+        const topo = secao.getBoundingClientRect().top
+        const alturaDaSecao = secao.offsetHeight
+
+        if (topo <= 150 && topo > -alturaDaSecao) {
+            menuLinks.forEach((link) => {
+                link.classList.remove("active")
+
+                if (link.getAttribute("href") === "#" + secao.id) {
+                    link.classList.add("active")
+                }
+            })
+        }
+    })
+})
+
+fecharMenu()
+
+btToggle.addEventListener("click", () => {
+    menu.classList.toggle("active")
+    btToggle.classList.toggle("active")
+})
+
+function fecharMenu() {
+    menuLinks.forEach((link) => {
+        link.addEventListener("click", () => {
+            menu.classList.remove("active")
+        })
+    })
+}
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) {
+        btVoltar.classList.add("active")
+    } else {
+        btVoltar.classList.remove("active")
+    }
+})
+
+btVoltar.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    })
+})
