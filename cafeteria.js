cf6f8a0518336ac3150807f4a4717e943768af41
caio5511 <caio.sousa.2504@gmail.com>
2026-07-28 20:44:03 -0300

    Finaliza responsividade e carrossel de depoimentos

--- cafeteria.js
+++ cafeteria.js
@@ -4,6 +4,61 @@ const menuLinks = document.querySelectorAll(".menu a")
 const btVoltar = document.querySelector(".back-to-top")
 const animarSecao = document.querySelectorAll(".animar");
 const secoes = document.querySelectorAll("main section")
+const depoimentosContainer = document.querySelector(".depoimentos-container")
+const slides = document.querySelectorAll(".depoimento-card");
+const indicadores = document.querySelectorAll(".indicador")
+
+let slideAtual = 0;
+
+function moverSlide() {
+    depoimentosContainer.style.transform = `translateX(${-(slideAtual * 100)}%)`
+
+    atualizarIndicadores()
+}
+
+function avancarSlide() {
+    slideAtual++
+
+    if (slideAtual >= slides.length) {
+        slideAtual = 0
+    }
+    moverSlide()
+}
+
+function iniciarCarrossel() {
+    intervalo = setInterval(() => {
+        avancarSlide();
+    }, 3000);
+}
+
+depoimentosContainer.addEventListener("mouseenter", () => {
+    clearInterval(intervalo)
+});
+
+depoimentosContainer.addEventListener("mouseleave", () => {
+    iniciarCarrossel()
+});
+
+function atualizarIndicadores() {
+    indicadores.forEach((indicador) => {
+        indicador.classList.remove("ativo")
+    })
+
+    indicadores[slideAtual].classList.add("ativo")
+
+}
+
+
+    indicadores.forEach((indicador, id) => {
+        indicador.addEventListener("click", () => {
+            slideAtual = id
+            moverSlide()
+        })
+    })
+
+    iniciarCarrossel()
+
+
 
 
 const observer = new IntersectionObserver((entries) => {
